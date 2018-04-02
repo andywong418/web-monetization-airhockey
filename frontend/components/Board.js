52bfba79ae976e56fff08dcc847f3e6b2f3e2dec
Andros Wong <androswong@awong-lm.local>
2018-04-02 14:08:29 -0700

    Make it move on the otherside

--- frontend/components/Board.js
+++ frontend/components/Board.js
@@ -9,6 +9,7 @@ class Board extends React.Component {
     super(props);
     const w = window.innerWidth;
     const h = window.innerHeight;
+    console.log(props.challenger);
     // Height should be set to be at least 400px
     // Width should be 70% of board.
     const leftOffset = w * 0.15;
@@ -42,7 +43,7 @@ class Board extends React.Component {
         [key]: newCoord
       });
     }
-    if((this.props.challenger && (key === 'player1X' || key === 'player1Y')) || (this.props.challenger && (key === 'player2X' || key === 'player2Y')) || (!this.props.challenger && key === 'ballX') || (!this.props.challenger && key === 'ballY') ) {
+    if((this.props.challenger && (key === 'player1X' || key === 'player1Y')) || (this.props.challenger && (key === 'player2X' || key === 'player2Y')) || (this.props.challenger && key === 'ballX') || (this.props.challenger && key === 'ballY') ) {
       // Broadcast to other side. Need to broadcast ball position as well.
       this.props.socket.emit('updateOtherPlayerCoords', {
         targetSocket: this.props.targetSocket,
